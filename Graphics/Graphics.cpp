69d2eb23d9ccae74cd90dc0ca7125fd0a988f81c
Stefan Raab <stefan.raab@htl-braunau.at>
2025-06-24 11:09:39 +0200

    hitmole

--- Graphics/Graphics.cpp
+++ Graphics/Graphics.cpp
@@ -2,28 +2,30 @@
 #include <conio.h>
 #include "graphics.h"
 
-#define SIZE		3
-#define TILE_SIZE	200
+#define SIZE		5
+#define TILE_SIZE	100
 
 struct IMAGES
 {
 	unsigned char* mole;
 	unsigned char* hole;
-	unsigned char* hammer;
 };
 
 void readImages(IMAGES* images); // Bilder in den Speicher laden
 void showImage(unsigned char* image, int row, int col);
 void initGamefield(int gamefield[][SIZE], IMAGES* images);
+bool HitMole(int gamefield[][SIZE], IMAGES* images, int row, int col, int& molesHit);
+
 
 
 void main()
 {
 	int window;
 	int gamefield[SIZE][SIZE];
-	//int mouseX;
-	//int mouseY;
+	int mouseX;
+	int mouseY;
 	IMAGES images;
+	int molesHit = 0;
 	
 	window = initwindow(1024, 768, "Whack-A-Mole");
 	setcurrentwindow(window);
@@ -32,9 +34,37 @@ void main()
 
 	initGamefield(gamefield, &images);
 
+	while (true)
+	{
+		if (ismouseclick(WM_LBUTTONDOWN))
+		{
+			//Linke Maustaste			
+			getmouseclick(WM_LBUTTONDOWN, mouseX, mouseY);
 
+			if (mouseX >= 0 && mouseX <= SIZE * TILE_SIZE && mouseY >= 0 && mouseY <= SIZE * TILE_SIZE)
+			{
+				//Mausposition ist im Spielfeld
+
+				int col = mouseX / TILE_SIZE;
+				int row = mouseY / TILE_SIZE;
+
+				if (HitMole(gamefield, &images, row, col, molesHit))
+				{
+					showImage(&images->hole, row, col);
+				}
+			}
+		}
+
+		Sleep(10);
+	}
 
 	_getch();
+}
+
+
+bool HitMole(int gamefield[][SIZE], IMAGES* images, int row, int col, int& molesHit)
+{
+
 }
 
 void initGamefield(int gamefield[][SIZE], IMAGES* images)
@@ -56,15 +86,11 @@ void showImage(unsigned char* image, int row, int col)
 
 void readImages(IMAGES* images)
 {
-	readimagefile(".\\Images\\Hole.bmp", 100, 100, 171, 171);
+	readimagefile(".\\Images\\Hole.bmp", 100, 100, 200, 200);
 	images->hole = (unsigned char*)malloc(imagesize(100, 100, 200, 200));
-	getimage(100, 100, 171, 171, images->hole);
+	getimage(100, 100, 200, 200, images->hole);
 
-	readimagefile(".\\Images\\Mole.bmp", 100, 100, 171, 171);
+	readimagefile(".\\Images\\Mole.bmp", 100, 100, 200, 200);
 	images->mole = (unsigned char*)malloc(imagesize(100, 100, 200, 200));
-	getimage(100, 100, 171, 171, images->mole);
-
-	readimagefile(".\\Images\\Hammer.bmp", 100, 100, 171, 171);
-	images->hammer = (unsigned char*)malloc(imagesize(100, 100, 200, 200));
-	getimage(100, 100, 171, 171, images->hammer);
+	getimage(100, 100, 200, 200, images->mole);
 }
