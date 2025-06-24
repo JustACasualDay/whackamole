7576ccdc60b3935382d061029c06bc358835de1a
Stefan Raab <stefan.raab@htl-braunau.at>
2025-06-24 09:39:21 +0200

    commit

--- Graphics/Graphics.cpp
+++ Graphics/Graphics.cpp
@@ -2,6 +2,9 @@
 #include <conio.h>
 #include "graphics.h"
 
+//#define SIZE		3
+//#define TILE_SIZE	
+
 struct IMAGES
 {
 	unsigned char* mole;
@@ -10,25 +13,42 @@ struct IMAGES
 };
 
 void readImages(IMAGES* images); // Bilder in den Speicher laden
+//void initGamefield(int gamefield[][SIZE]);
 
 
 void main()
 {
 	int window;
+	//int gamefield[SIZE][SIZE];
+	//int mouseX;
+	//int mouseY;
+	IMAGES images;
 	
-	window = initwindow(1024, 768, "Grafiktest");
+	window = initwindow(1024, 768, "Whack-A-Mole");
 	
 	setcurrentwindow(window);
 
-	line(0, 0, getmaxx(), getmaxy());
+	readImages(&images);
 
 	_getch();
 }
 
+//void initGamefield(int gamefield[][SIZE])
+//{
+//
+//}
+
 void readImages(IMAGES* images)
 {
-	readimagefile(".\\Images\\", 100, 100, 171, 171);
-	images->unopened = (unsigned char*)malloc(imagesize(100, 100, 171, 171));
-	getimage(100, 100, 171, 171, images->unopened);
+	readimagefile(".\\Images\\Hole.png", 100, 100, 171, 171);
+	images->hole = (unsigned char*)malloc(imagesize(100, 100, 171, 171));
+	getimage(100, 100, 171, 171, images->hole);
 
+	readimagefile(".\\Images\\Mole.png", 100, 100, 171, 171);
+	images->mole = (unsigned char*)malloc(imagesize(100, 100, 171, 171));
+	getimage(100, 100, 171, 171, images->mole);
+
+	readimagefile(".\\Images\\Hammer.png", 100, 100, 171, 171);
+	images->hammer = (unsigned char*)malloc(imagesize(100, 100, 171, 171));
+	getimage(100, 100, 171, 171, images->hammer);
 }
