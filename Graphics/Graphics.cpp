53514131ab0f86e276d6b8fdbdfff06aef127c47
JustACasualDay <samuel.kaltenegger@gmail.com>
2025-06-24 09:59:24 +0200

    Added a few Stuff

--- Graphics/Graphics.cpp
+++ Graphics/Graphics.cpp
@@ -2,8 +2,8 @@
 #include <conio.h>
 #include "graphics.h"
 
-//#define SIZE		3
-//#define TILE_SIZE	
+#define SIZE		3
+#define TILE_SIZE	70
 
 struct IMAGES
 {
@@ -13,30 +13,46 @@ struct IMAGES
 };
 
 void readImages(IMAGES* images); // Bilder in den Speicher laden
-//void initGamefield(int gamefield[][SIZE]);
+void showImage(unsigned char* image, int row, int col);
+void initGamefield(int gamefield[][SIZE], IMAGES* images);
 
 
 void main()
 {
 	int window;
-	//int gamefield[SIZE][SIZE];
+	int gamefield[SIZE][SIZE];
 	//int mouseX;
 	//int mouseY;
 	IMAGES images;
 	
 	window = initwindow(1024, 768, "Whack-A-Mole");
-	
 	setcurrentwindow(window);
 
 	readImages(&images);
 
+	initGamefield(gamefield, &images);
+
+
+
 	_getch();
 }
 
-//void initGamefield(int gamefield[][SIZE])
-//{
-//
-//}
+void initGamefield(int gamefield[][SIZE], IMAGES* images)
+{
+	for (int row = 0; row < SIZE; row++)
+	{
+		for (int col = 0; col < SIZE; col++)
+		{
+			showImage(images->hole, row, col);
+		}
+	}
+}
+
+
+void showImage(unsigned char* image, int row, int col)
+{
+	putimage(col * TILE_SIZE, row * TILE_SIZE, image, COPY_PUT);
+}
 
 void readImages(IMAGES* images)
 {
